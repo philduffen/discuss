6217b8c2c7087c7546285c2a37398c9c8a987f99
Phil Duffen <phil@duffen.co.uk>
2018-10-03 15:56:53 +0100

    Add new function to topic_controller. Add view/template for topic new Add post create route for topic. Update topic model.

--- web/controllers/topic_controler.ex
+++ web/controllers/topic_controler.ex
@@ -1,7 +1,15 @@
 defmodule Discuss.TopicController do
   use Discuss.Web, :controller
 
-  def new(conn, params) do
-    render(conn, "new.html")
+  # same as 'alias Discuss.Topic, as: Topic'
+  alias Discuss.Topic
+
+  def new(conn, _params) do
+    changeset = Topic.changeset(%Topic{}, %{})
+
+    render(conn, "new.html", changeset: changeset)
+  end
+
+  def create(conn, %{"topic " => topic}) do
   end
 end
