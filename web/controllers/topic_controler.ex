794ed12001038689e8c4ba70f58fdb2957754587
Phil Duffen <phil@duffen.co.uk>
2018-10-04 14:47:34 +0100

    Add debug to topic create function - with Repo.insert.

--- web/controllers/topic_controler.ex
+++ web/controllers/topic_controler.ex
@@ -4,12 +4,18 @@ defmodule Discuss.TopicController do
   # same as 'alias Discuss.Topic, as: Topic'
   alias Discuss.Topic
 
-  def new(conn, _params) do
+  def new(conn, params) do
     changeset = Topic.changeset(%Topic{}, %{})
 
     render(conn, "new.html", changeset: changeset)
   end
 
-  def create(conn, %{"topic " => topic}) do
+  def create(conn, %{"topic" => topic}) do
+    changeset = Topic.changeset(%Topic{}, topic)
+
+    case Repo.insert(changeset) do
+      {:ok, post} -> IO.inspect(post)
+      {:error, changeset} -> IO.inspect(changeset)
+    end
   end
 end
