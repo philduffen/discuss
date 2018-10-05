ffa70df0f9d99464c5ce11e42be18e07356c56ab
Phil Duffen <phil@duffen.co.uk>
2018-10-05 10:06:46 +0100

    Redirect to new form on topic insert error. Add css for form and error message.

--- web/controllers/topic_controler.ex
+++ web/controllers/topic_controler.ex
@@ -14,8 +14,11 @@ defmodule Discuss.TopicController do
     changeset = Topic.changeset(%Topic{}, topic)
 
     case Repo.insert(changeset) do
-      {:ok, post} -> IO.inspect(post)
-      {:error, changeset} -> IO.inspect(changeset)
+      {:ok, post} ->
+        IO.inspect(post)
+
+      {:error, changeset} ->
+        render(conn, "new.html", changeset: changeset)
     end
   end
 end
