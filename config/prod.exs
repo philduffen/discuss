7523a352be53e5938f75a1ba8324f574bfcf3f8a
Phil Duffen <phil@duffen.co.uk>
2018-10-09 13:37:17 +0100

    Remove manifest.json reference in prod.exs

--- config/prod.exs
+++ config/prod.exs
@@ -32,7 +32,7 @@ config :discuss, Discuss.Endpoint,
   http: [:inet6, port: {:system, "PORT"}],
   url: [scheme: "https", host: "vast-plateau-43862.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
-  cache_static_manifest: "priv/static/manifest.json",
+  # cache_static_manifest: "priv/static/manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 # Configure your database
