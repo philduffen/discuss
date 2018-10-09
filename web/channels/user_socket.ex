ad6e9875e0209579b5b2f5cc69efe46e6329faee
Phil Duffen <phil@duffen.co.uk>
2018-10-09 12:16:34 +0100

    Update user_socket.ex

--- web/channels/user_socket.ex
+++ web/channels/user_socket.ex
@@ -5,10 +5,8 @@ defmodule Discuss.UserSocket do
   # channel "room:*", Discuss.RoomChannel
 
   ## Transports
-  transport(:websocket, Phoenix.Transports.WebSocket,
-    # transport :longpoll, Phoenix.Transports.LongPoll
-    timeout: 45_000
-  )
+  transport(:websocket, Phoenix.Transports.WebSocket, timeout: 45_000)
+  # transport :longpoll, Phoenix.Transports.LongPoll
 
   # Socket params are passed from the client and can
   # be used to verify and authenticate a user. After
