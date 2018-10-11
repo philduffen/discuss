01d4d741498935f8e60c4f09d2f8c38af6bab518
Phil Duffen <phil@duffen.co.uk>
2018-10-11 09:13:28 +0100

    Add plug SetUser.

---
+++ web/controllers/plugs/set_user.ex
@@ -0,0 +1,23 @@
+defmodule Discuss.plugs().SetUser do
+  import Plug.Conn
+  import Phoenix.Controller
+
+  alias Discuss.Repo
+  alias Discuss.User
+
+  def init(_params) do
+  end
+
+  # _params is returned from init function
+  def call(conn, _params) do
+    user_id = get_session(conn, :user_id)
+
+    cond do
+      user = user_id && Repo.get(User, user_id) ->
+        assign(conn, :user, user)
+
+      true ->
+        assign(conn, :user, nil)
+    end
+  end
+end
